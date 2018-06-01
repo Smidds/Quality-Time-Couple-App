e1adfc1aec84f2913fadfb65b0a0ab5df241d805
Isaac Smith <isaacsmith@isaacs-macbook.attlocal.net>
2018-06-01 10:50:33 -0500

    Begin creating sign-up form

--- lib/pages/sign-up.dart
+++ lib/pages/sign-up.dart
@@ -11,20 +11,70 @@ class SignUpFormState extends State<SignUpForm> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Padding(
-      padding: const EdgeInsets.all(50.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
-          color: Colors.lightBlue,
-          boxShadow: [
-            BoxShadow(
-              spreadRadius: 1.0,
-              blurRadius: 2.0,
-              color: Colors.grey,
-            )
-          ],
+    return Container(
+      color: const Color.fromRGBO(228, 143, 239, 100.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 75.0),
+        child: new Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60.0),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      spreadRadius: 7.0,
+                      blurRadius: 10.0,
+                      color: Colors.black26)
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50.0),
+                child: new Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: new Column(
+                    children: <Widget>[
+                      Text(
+                        "Quality Time",
+                        style: TextStyle(fontSize: 25.0, color: Colors.black),
+                        textAlign: TextAlign.center,
+                        textDirection: TextDirection.ltr,
+                      ),
+                      Padding(padding: const EdgeInsets.all(5.0)),
+                      Text(
+                        "A Couples App",
+                        style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontSize: 14.0,
+                            color: Colors.purpleAccent),
+                        textAlign: TextAlign.center,
+                        textDirection: TextDirection.ltr,
+                      )
+                    ],
+                  ),
+                ),
+              )
+              // child: TextInput(
+              //     label: "Email: ", errorMessage: "Please enter your email."),
+              ),
         ),
+      ),
+    );
+  }
+}
+
+class TextInput extends StatelessWidget {
+  final String label, errorMessage;
+
+  TextInput({@required this.label, @required this.errorMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        children: <Widget>[Text("Hello World!")],
       ),
     );
   }
